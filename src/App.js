1b21228b73c2d75d57ecda4674271324840a06b2
ryudaibaba <1844ryudai@gmail.com>
2021-10-15 00:38:01 +0900

    todoパラメータ渡し

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ const App = () => {
     price: 1000,
   }
 
-  const [name, setName] = useState(initialStates.name)
-  const [price, setPrice] = useState(initialStates.price)
+  const [state, setState] = useState({...initialStates})
+
+  // パラメータをこれで渡せるはず
+  const param = state
+  console.log(param)
 
   const reset = () => {
-    setPrice(1000)
-    setName('')
+    setState({...initialStates})
   }
 
   return (
     <>
-      <p>現在の{name}は、{price}円です</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <p>現在の{state.name}は、{state.price}円です</p>
       <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)} />
+
+      <input value={state.name} onChange={e => setState({...state, name: e.target.value})} />
+
+      <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
     </>
   )
 }
